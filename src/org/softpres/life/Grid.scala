c49a623cd1e42eb0f4846a43f9667e5196a8fed8
kieron <kieron@a5a2c0e3-4c5c-47ae-be6d-43dadaffd7e4>
2011-10-03 07:13:58 +0000

    optimised Scala grid, converted from an optimised Java version
    git-svn-id: http://myth/svn/gameoflife@591 a5a2c0e3-4c5c-47ae-be6d-43dadaffd7e4

---
+++ src/org/softpres/life/Grid.scala
@@ -0,0 +1,134 @@
+package org.softpres.life
+
+/**
+ * Scala GOL grid, post optimisation.
+ *
+ * @author kieron
+ */
+class Grid(dimension: Int) {
+
+  private var grid = new Array[Boolean](dimension * dimension)
+  private var buffer = new Array[Boolean](dimension * dimension)
+
+  private def swap() {
+    val tmp = grid
+    grid = buffer
+    buffer = tmp
+  }
+
+  private def x(i: Int) = (i % dimension) + 1
+  private def y(i: Int) = (i / dimension) + 1
+
+  private def index(x: Int, y: Int) = (y - 1) * dimension + (x - 1)
+
+  private def cell(x: Int, y: Int): Boolean = {
+    if (x <= 0 || x > dimension || y <= 0 || y > dimension) {
+      return false
+    }
+    return grid(index(x, y))
+  }
+
+  private def prime(x: Int, y: Int, alive: Boolean) {
+    buffer(index(x, y)) = alive
+  }
+
+  def activate(x: Int, y: Int) {
+    grid(index(x, y)) = true
+  }
+
+  private def neighbours(x: Int, y: Int): Int = {
+    return neighbour(x, y, -1, -1) +
+           neighbour(x, y, -1,  0) +
+           neighbour(x, y, -1,  1) +
+           neighbour(x, y,  0, -1) +
+           neighbour(x, y,  0,  1) +
+           neighbour(x, y,  1, -1) +
+           neighbour(x, y,  1,  0) +
+           neighbour(x, y,  1,  1)
+  }
+
+  private def neighbour(x: Int, y: Int, xd: Int, yd: Int): Int = {
+    return if (cell(x + xd, y + yd)) 1 else 0
+  }
+
+  final def tick() {
+    var y = 1
+    while (y <= dimension) {
+      var x = 1
+      while (x <= dimension) {
+        val before = cell(x, y)
+        val n = neighbours(x, y)
+        val after = alive(before, n)
+        prime(x, y, after)
+        x += 1
+      }
+      y += 1
+    }
+    swap
+  }
+
+  private def alive(alive: Boolean, neighbours: Int): Boolean = {
+    if (alive) {
+      if (neighbours <  2) return false
+      if (neighbours <= 4) return true
+    }
+    else {
+      return neighbours == 3
+    }
+    return false
+  }
+
+  def count: Int = {
+    var sum = 0
+    for (cell <- grid) {
+      sum += (if (cell) 1 else 0)
+    }
+    return sum
+  }
+
+  override def toString: String = {
+    val coords = for {
+      x <- 1 to dimension
+      y <- 1 to dimension
+      if cell(x, y)
+    } yield x + "," + y
+
+    coords.sorted.mkString("(", "), (", ")")
+  }
+
+}
+
+object GridDemo {
+
+  def main(args: Array[String]): Unit = {
+    val grid = new Grid(10)
+    grid.activate(1, 2)
+    grid.activate(2, 3)
+    grid.activate(3, 1)
+    grid.activate(3, 2)
+    grid.activate(3, 3)
+
+//    for (_ <- 1 to 5) time(grid)
+    print(grid)
+  }
+
+  private def time(grid: Grid): Unit = {
+    val start = System.currentTimeMillis
+    var i = 1
+    while (i <= 1000000) {
+      grid.tick()
+      i += 1
+    }
+    val end = System.currentTimeMillis
+    System.out.println(end - start)
+  }
+
+  private def print(grid: Grid): Unit = {
+    System.out.println(grid)
+    for (_ <- 1 to 20) {
+      grid.tick()
+      println(grid.count + ": " + grid)
+    }
+  }
+
+}
